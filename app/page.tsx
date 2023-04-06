14427ebb30231163dd48759624d415872d60641d
xizhen <xuxizhen@gmail.com>
2023-04-06 22:48:57 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import { Home } from "./components/home";
 export default function App() {
   return (
     <>
+      <div>
       <Home />
+      <div style={{textAlign:"center", fontSize:"12px", padding:"9px 0"}}>Join Telegram group | 加入电报组: <a href="https://t.me/chatgptv12345">https://t.me/chatgptv12345</a></div>
+      <div>
       <Analytics />
     </>
   );
