6029aa61352b4d0c81afa28fd92c64823d752ae6
xizhen <xuxizhen@gmail.com>
2023-04-06 18:29:40 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,15 +49,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KY2PKZDJDC"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-KY2PKZDJDC');
-        </script>
         <meta
           name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
