303f621f22ecea416ffd49e9ada18d2607b47db8
xizhen <xuxizhen@gmail.com>
2023-04-06 22:02:33 +0800

    Update serviceWorkerRegister.js

--- public/serviceWorkerRegister.js
+++ public/serviceWorkerRegister.js
@@ -7,9 +7,3 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
-
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-KY2PKZDJDC');
