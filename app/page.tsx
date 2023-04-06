993c5be38043679eacf9e1b9145ff59cd3d6ef27
xizhen <xuxizhen@gmail.com>
2023-04-06 23:08:40 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function App() {
         <div style={{textAlign:"center", fontSize:"12px", padding:"9px 0"}}>
         Join Telegram group | 加入电报组: <a href="https://t.me/chatgptv12345">https://t.me/chatgptv12345</a>
         </div>
-      <div>
+      </div>
       <Analytics />
     </>
   );
