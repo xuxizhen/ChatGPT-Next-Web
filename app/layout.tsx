a6781f52012b3a113015856c1a5493a990ff156d
xizhen <xuxizhen@gmail.com>
2023-04-06 21:08:57 +0800

    Create layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="/ga.js" defer></script>
         <meta
           name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
